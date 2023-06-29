35397ecffbc3af596181910d88df90cfcd9c8ac7
Eric Junker <eric.junker@dice.com>
2023-06-29 15:28:41 -0500

    add $routeGroups property to a RouteGroup so that it can inherit settings from other route groups

--- src/RouteGroup.php
+++ src/RouteGroup.php
@@ -14,15 +14,27 @@ abstract class RouteGroup
 
     public array $where = [];
 
+    public array $routeGroups = [];
+
     public function toArray(): array
     {
-        return array_filter([
+        $groupArrays = [];
+        foreach ($this->routeGroups as $routeGroup) {
+            $groupArrays[$routeGroup] = (new $routeGroup)->toArray();
+        }
+
+        $groupArrays['inline'] = array_filter([
             'prefix' => $this->prefix,
             'as' => $this->as,
             'domain' => $this->domain,
             'middleware' => array_unique([...$this->middleware, ...$this->middleware()]),
             'where' => $this->where,
         ]);
+
+        // merge route attributes from route groups
+        return array_reduce($groupArrays, static function ($actionArray, $group) {
+            return \Illuminate\Routing\RouteGroup::merge($group, $actionArray, prependExistingPrefix: true);
+        }, []);
     }
 
     public function middleware(): array
